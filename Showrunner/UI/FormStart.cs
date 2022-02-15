f0fd256d2e92b25040b0a97e98ea4e3f60884d90
Justice Ben-Ezra <justicedbenezra@gmail.com>
2022-02-15 11:57:49 -0600

    Created Start Modern UI

--- Showrunner/UI/FormStart.cs
+++ Showrunner/UI/FormStart.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,7 +22,7 @@ namespace Showrunner.UI
         public FormStart()
         {
             InitializeComponent();
-            Text = "Showrunner " + Program.version; // Display version.
+            //Text = "Showrunner " + Program.version; // Display version.
 
             if (!Directory.Exists(showrunnerPath))
             {
@@ -113,6 +114,21 @@ namespace Showrunner.UI
                     Visible = false;
                 }
             }
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
