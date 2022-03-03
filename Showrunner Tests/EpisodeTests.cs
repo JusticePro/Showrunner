b560bc855c0e56d3fdec4c9b470cbeb56f33a0be
Justice Ben-Ezra <justicedbenezra@gmail.com>
2022-03-03 13:44:21 -0600

    Created Test Project. Removed the obsolute save and loading system in Episode and Show.

---
+++ Showrunner Tests/EpisodeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Showrunner;
+using System;
+
+namespace Showrunner_Tests
+{
+    [TestClass]
+    public class EpisodeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange the Test
+            Episode episode = new Episode();
+
+            // Perform the Action (Act)
+
+
+            // Assert the Result
+        }
+    }
+}
---
+++ Showrunner Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Showrunner Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Showrunner Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e38548f9-3733-406a-a36d-1ab418099787")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
