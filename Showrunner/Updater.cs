fceecbf1c5e760ba2afa26a574aeb4e088f61adc
Justice Ben-Ezra <justicedbenezra@gmail.com>
2022-03-07 13:32:09 -0600

    Updated Version Data

--- Showrunner/Updater.cs
+++ Showrunner/Updater.cs
@@ -11,8 +11,8 @@ namespace Showrunner
 {
     public class Updater
     {
-        private static string version = "1.1.3";
-        private static int versionID = 6;
+        private static string version = "1.2";
+        private static int versionID = 7;
         
         private static string updateDetailURL =
             "https://raw.githubusercontent.com/JusticePro/Showrunner/master/Showrunner/update_details.txt";
